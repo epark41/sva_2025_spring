19ffc90c964d938900552d1a15cc25d846fab834
Soo Park <epark41@sva.edu>
2025-01-30 20:10:58 -0500

    mutiple evnets

--- week_03/sketch.js
+++ week_03/sketch.js
@@ -1,5 +1,30 @@
+let links = [];
 function setup(){
-    createCanvas(800,800);
-    background(0);
-    circle(100,100,100);
+    for (let i=0; i<50; i++){
+        const link = createA("#", "Yahoo!");
+    
+    link.position(random(500), random(500));
+    link.mouseClicked(onMouseClick);
+    link.mouseOver(onMouseOver);
+    link.mouseOut(onMouseOut);
+    links.push(link);
+    }
+}
+
+function onMouseClick(){
+    this.style("background-color", "green");
+    this.style("color", "white");
+
+}
+
+function onMouseOver(){
+    this.style("background-color", "pink");
+    this.style("color", "green");
+
+}
+
+function onMouseOut(){
+    this.style("background-color", "yellow");
+    this.style("color", "black");
+
 }
