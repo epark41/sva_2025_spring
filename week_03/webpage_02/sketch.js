b46c570cfcd8084a8b9b90b549d65e2932a1437b
Soo Park <epark41@sva.edu>
2025-02-05 16:12:10 -0500

    basic set

--- week_03/webpage_02/sketch.js
+++ week_03/webpage_02/sketch.js
@@ -1,32 +1,29 @@
 function setup(){
-    createCanvas(800,800);
-    background(100);
 
-    circle(width/2,height/2,400);
-    const sideNavItem = selectAll('.side-nav-item');
-    for (let i=0; i<sideNavItem.length; i++){
-        sideNavItem[i].mousePressed(onMousePressed);
-        sideNavItem[i].mouseOver(onMouseOver);
-        sideNavItem[i].mouseOut(onMouseOut);
-    }
+const sideNavItem = selectAll('.side-nav-item');
+for (let i=0; i<sideNavItem.length; i++){
+    sideNavItem[i].mousePressed(onMousePressed);
+    sideNavItem[i].mouseOver(onMouseOver);
+    sideNavItem[i].mouseOut(onMouseOut);
+}
 }
 
 function onMousePressed(){
-   const id = this.elt.dataset.id;
-   const className = "." + id;
-   const projects = selectAll('.project');
-   for (let i=0 ; i< projects.length; i++){
-    projects[i].hide();
-   }
-   const selectedProject = select(className);
-   selectedProject.show();
+const id = this.elt.dataset.id;
+const className = "." + id;
+const projects = selectAll('.project');
+for (let i=0 ; i< projects.length; i++){
+projects[i].hide();
+}
+const selectedProject = select(className);
+selectedProject.show();
 }
 
 function onMouseOver(){
-    this.style("color","red");
+this.style("color","red");
 }
 
 function onMouseOut(){
-    this.style("color","Black");
-
+this.style("color","Black");
 }
+
