32b4d95931980be7e325060a079940c31f2bd339
Soo Park <epark41@sva.edu>
2025-02-27 20:02:24 -0500

    excercise

---
+++ week_07_matterjs/sketch.js
@@ -0,0 +1,42 @@
+let engine;
+let boxes = [];
+let boxSize = 30;
+
+function setup() {
+    createCanvas(600, 600);
+    rectMode(CENTER);
+
+    engine = Matter.Engine.create();
+    let ground = Matter.Bodies.rectangle(
+        width / 2, height, width * 1.25, 25, { isStatic: true }
+    );
+    Matter.Composite.add(engine.world, ground);
+}
+
+function draw() {
+    background(0);
+    
+    for (let i = 0; i < boxes.length; i++) {
+        const box = boxes[i];
+        push();
+        fill(0);
+        stroke(255);
+        translate(box.position.x, box.position.y);
+        rotate(box.angle);
+        rect(0,0, boxSize, boxSize);
+        pop();
+    }
+
+    Matter.Engine.update(engine, 1000 / 30);
+}
+
+function addBox(x, y) {
+    const box = Matter.Bodies.rectangle(x, y, boxSize, boxSize);
+    Matter.Composite.add(engine.world, box);
+    boxes.push(box);
+
+}
+
+function mousePressed() {
+    addBox(mouseX, mouseY);
+}
