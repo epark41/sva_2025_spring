8052aea0e6e0d1c2a56d1420d2976af89aea7950
Soo Park <epark41@sva.edu>
2025-04-03 21:34:55 -0400

    class_11

---
+++ week_11_citybike/station.js
@@ -0,0 +1,23 @@
+class Station {
+    constructor(data){
+        this.isValid = true;
+        this.id = data.id;
+        const x = map(data.lng,minLng,maxLng,width,0);
+        const y = map(data.lat,minLat,maxLat,height,0);
+        this.pos = createVector(x,y);
+
+        if(x>width || y > height){
+            this.isValid = false;
+        }
+    }
+
+    getPos(){
+        return this.pos.copy();
+    }
+
+    display(){
+        noStroke();
+        fill(100);
+        circle(this.pos.x,this.pos.y,5);
+    }
+}
